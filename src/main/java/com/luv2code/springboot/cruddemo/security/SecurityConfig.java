418713443da99846e854da5f8774fb4225197cfe
RakeshkumarThera <rakeshkumarthera@gmail.com>
2024-11-04 17:18:43 -0500

    role based access

--- src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -30,5 +35,27 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(rakz, reshma, ravi);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+                );
+
+        //use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF)
+        //CSRF is only for webforms or webdevelopment and not required for internal usage
+        http.csrf(csrf -> csrf.disable());
+
+        return http.build();
     }
 }
